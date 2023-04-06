6c94ce99275e6b9cc96eb1c94c1eeb377d042ef4
Dewey Watkins <deweywatkins77@gmail.com>
2023-04-06 01:23:08 -0400

    add id field to db.json, added id = uuid to /api/notes post route

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -84,6 +84,7 @@ const handleNoteDelete = (e) => {
 
   const note = e.target;
   const noteId = JSON.parse(note.parentElement.getAttribute('data-note')).id;
+  console.log(noteId)
 
   if (activeNote.id === noteId) {
     activeNote = {};
@@ -161,7 +162,7 @@ const renderNoteList = async (notes) => {
   jsonNotes.forEach((note) => {
     const li = createLi(note.title);
     li.dataset.note = JSON.stringify(note);
-
+    console.log(JSON.stringify(note))
     noteListItems.push(li);
   });
 
--- server.js
+++ server.js
@@ -1,28 +1,30 @@
 const express = require('express')
 const PATH = require('path')
-const app =express()
 const { readFromFile, readAndAppend } = require('./helpers/fsUtils');
+const uuid = require('./helpers/uuid')
+const app =express()
 const PORT = process.env.PORT || 3001
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static('public'));
 
 app.get('/notes', (req, res) => {
     res.sendFile(__dirname + "/public/notes.html")
 })
+
 app.get('/api/notes', (req, res) => {
     readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
-  })
+})
 
 app.post('/api/notes', (req, res) => {
-    const newNote = req.body
+    let newNote = req.body
+    newNote.id = uuid()
     readAndAppend(newNote, './db/db.json')
 })
 
-app.delete('/api/notes', (req, res) => {
-    let currentData 
-    readFromFile('./db/db.json').then((data) => console.log(data))
+app.delete('/api/notes/:id', (req, res) => {
+    readFromFile('./db/db.json').then((data) => console.log(JSON.parse(data)))
 })
 
 app.listen(PORT, () =>
